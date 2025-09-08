628ca75f654fa67015fc985befdd193c2ab01a92
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-09-08 16:59:01 +0800

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Route, Routes} from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import NotePage from './pages/NotePage.jsx' 
 
 const App = () => {
   return (
     <div>
-      App works!
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NotePage />} />
+      </Routes>
     </div>
   )
 }
