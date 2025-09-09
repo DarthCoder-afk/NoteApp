cf820fb2d4216cf81e97c254ef8d51f6b2803f21
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-09-09 22:47:20 +0800

    daisyui for components

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,10 @@ import { toast } from 'react-hot-toast'
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <button className="px-4 py-2 border border-gray-300 rounded-md drop-shadow hover:bg-gray-200" 
-      onClick={() => toast.success("Nicely done")}>Click Me</button>
+    <div>
+        <h1>
+            HomePage
+        </h1>
     </div>
   )
 }
