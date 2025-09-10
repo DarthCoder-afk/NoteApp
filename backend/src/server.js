8600dbd0695bb46fa6e355b3cdb8010c602e65f7
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-09-10 16:57:30 +0800

    added cors package

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';  
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';    
 import rateLimiter from './middleware/rateLimiter.js';
+
 
 dotenv.config();
 
@@ -15,6 +17,10 @@ app.use(express.json());
 
 // rate limiter middleware
 app.use(rateLimiter)
+
+app.use(cors({
+     origin: "http://localhost:5173"
+}))
 
 // app.use((req, res, next) => {
 //     console.log(`The request method is: ${req.method} and the request URL is: ${req.url}`);
